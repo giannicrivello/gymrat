e12751cd3d6314cf7cb27b3eb08f9804edd6f00d
gianni <gianni.crivello96@gmail.com>
2022-08-07 06:20:13 -0700

    got loader working

--- gymrat.cpp
+++ gymrat.cpp
@@ -10,14 +10,12 @@
 #include<numeric>
 
 
-using namespace std;
 //read in the workouts (key, val) ?
 //read in the idx (place holder for idx of key val)
 //construct a vector of workouts
 //return vector of workouts
 //write idx to file
 //
-
 class workout {
 	private:
 		std::map<std::string, std::string> map; //[name] = currentmax
@@ -27,19 +25,19 @@ class workout {
 	public:
 		workout(std::string filename, std::string idxname){
 			_filename = filename;
-			string line;
-			deque<string> queue;
-			ifstream file(filename);
+			std::string line;
+			std::deque<std::string> queue;
+			std::ifstream file(filename);
 			if(file.is_open()){
 				while(!file.eof()) {
 					while(getline (file, line)){
-						stringstream ss(line);
-						string s;
+						std::stringstream ss(line);
+						std::string s;
 						while(getline(ss, s, ' ')) {
 							queue.push_back(s);
 						}
-						string key = queue.at(0);
-						string val = queue.at(1);
+						std::string key = queue.at(0);
+						std::string val = queue.at(1);
 						map[key] = val;
 						//clear queue
 						queue.pop_front();
@@ -48,7 +46,7 @@ class workout {
 				}
 				file.close();
 			}
-			ifstream idxfile(idxname);
+			std::ifstream idxfile(idxname);
 			_idxfilename = idxname;
 			if(idxfile.is_open()){
 				getline(idxfile, line);
@@ -58,27 +56,33 @@ class workout {
 		}
 		~workout(){
 			idx++;
-			ofstream file(_idxfilename);
-			file << std::to_string(idx);
+			std::ofstream file(_idxfilename);
+			file << std::to_string(idx + 1);
 		}
 	void dump_all_workouts();
 	std::vector<std::string> load(int load_num);
 };
 std::vector<std::string> workout::load(int load_num){
-		std::vector<std::string> vec;
-		int n=0;
-		std::map<std::string, std::string>::iterator it=map.begin();
-		if(idx >= map.size()){
-			it = map.begin();
-		}else {
-			while(int i=0 < idx) it++;
-		}
-		while(n < load_num){
-			if(it == map.end()) it=map.begin();
-			vec.push_back(it->first);
-			n++;
-		}
-		return vec;
+	std::vector<std::string> vec;
+	//check if idx is out of bounds
+	//load in range load_num
+	//increment idx
+	int n=0;
+	std::map<std::string, std::string>::iterator it = map.begin();
+	//increment our pointer to last saved idx
+	for(int i=0; i < idx; i++) {
+		if(it == map.end()) it=map.begin();
+		it++;
+	}
+	while(n < load_num){
+		if(it == map.end()) it=map.begin();
+		vec.push_back(it->second);
+		it++;
+		n++;
+
+	}
+	return vec;
+
 }
 void workout::dump_all_workouts(){
 	for(std::map<std::string, std::string>::iterator it=map.begin(); it != map.end(); it++){
@@ -86,13 +90,13 @@ void workout::dump_all_workouts(){
 	}
 }
 int main(){
+	//keeping track of idx and workout file
 	workout chest("chest.txt", "chestidx.txt");
 	chest.dump_all_workouts();
-	std::vector<std::string> vec = chest.load(2);
+	std::vector<std::string> vec = chest.load(3);
 	for(std::vector<std::string>::iterator it=vec.begin(); it != vec.end(); it++){
 		std::cout << *it << std::endl;
 	}
-
 }
 
 
